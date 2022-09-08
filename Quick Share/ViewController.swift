8cf91a38dad33c522b2195550774863e41cace78
Nainaaas <shahina.sikkander@gmail.com>
2022-09-08 10:30:23 +0400

    Initial Commit

---
+++ Quick Share/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quick Share
+//
+//  Created by shahina kassim on 08/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
